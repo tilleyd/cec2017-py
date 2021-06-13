0c241240bfaf11d0f42a607a235b1825ad11e4f7
Łukasz Wolanin <l.wolanin@stud.elka.pw.edu.pl>
2021-06-13 02:34:40 +0200

    Fixed setup.py adn MANIFEST.in to include data.pkl in installation

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
+
+
 setup(name='CEC2017',
       version='1.0',
       description='CEC 2017 single objective optimization suite',
       author='Duncan Tilley',
       url='https://github.com/tilleyd/cec2017-py',
-      packages=['cec2017']
+      packages=['cec2017'],
+      include_package_data=True,
 )
+
