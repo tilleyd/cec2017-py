9efb5bb452783dbf4f34a1446ae62f70c5f22ee4
Duncan Tilley <duncan.tilley@gmail.com>
2021-09-11 07:11:56 +0200

    Add numpy as install requirement

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-
-setup(name='CEC2017',
-      version='1.0',
-      description='CEC 2017 single objective optimization suite',
-      author='Duncan Tilley',
-      url='https://github.com/tilleyd/cec2017-py',
-      packages=['cec2017'],
-      include_package_data=True,
+setup(
+    name="CEC2017",
+    version="1.0",
+    description="CEC 2017 single objective optimization suite",
+    author="Duncan Tilley",
+    url="https://github.com/tilleyd/cec2017-py",
+    packages=["cec2017"],
+    include_package_data=True,
+    install_requires=["numpy"],
 )
-
